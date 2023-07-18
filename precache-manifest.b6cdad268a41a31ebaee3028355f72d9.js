e72bc54585b2677e73e920ad2c49d6b078b0e852
EvanPiro <dev.evanpiro@gmail.com>
2023-07-17 22:24:04 -0400

    Updates

---
+++ precache-manifest.b6cdad268a41a31ebaee3028355f72d9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b056f8dafd63de25121d18649e166ff4",
+    "url": "ANDREWBTC707.github.io/favicon.jpeg"
+  },
+  {
+    "revision": "69045d03afdf61aeb37246af6001af9c",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/OFL.txt"
+  },
+  {
+    "revision": "14d00dab1f6802e787183ecab5cce85e",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Black.ttf"
+  },
+  {
+    "revision": "e9c5c588e39d0765d30bcd6594734102",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-BlackItalic.ttf"
+  },
+  {
+    "revision": "08c20a487911694291bd8c5de41315ad",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Bold.ttf"
+  },
+  {
+    "revision": "19406f767addf00d2ea82cdc9ab104ce",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-BoldItalic.ttf"
+  },
+  {
+    "revision": "d45bdbc2d4a98c1ecb17821a1dbbd3a4",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-ExtraBold.ttf"
+  },
+  {
+    "revision": "8afe4dc13b83b66fec0ea671419954cc",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "6f8391bbdaeaa540388796c858dfd8ca",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-ExtraLight.ttf"
+  },
+  {
+    "revision": "a9bed017984a258097841902b696a7a6",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-ExtraLightItalic.ttf"
+  },
+  {
+    "revision": "c1034239929f4651cc17d09ed3a28c69",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Italic.ttf"
+  },
+  {
+    "revision": "fcc40ae9a542d001971e53eaed948410",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Light.ttf"
+  },
+  {
+    "revision": "0613c488cf7911af70db821bdd05dfc4",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-LightItalic.ttf"
+  },
+  {
+    "revision": "bf59c687bc6d3a70204d3944082c5cc0",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Medium.ttf"
+  },
+  {
+    "revision": "cf5ba39d9ac24652e25df8c291121506",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-MediumItalic.ttf"
+  },
+  {
+    "revision": "093ee89be9ede30383f39a899c485a82",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Regular.ttf"
+  },
+  {
+    "revision": "6f1520d107205975713ba09df778f93f",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-SemiBold.ttf"
+  },
+  {
+    "revision": "9841f3d906521f7479a5ba70612aa8c8",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "9ec263601ee3fcd71763941207c9ad0d",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-Thin.ttf"
+  },
+  {
+    "revision": "01555d25092b213d2ea3a982123722c9",
+    "url": "ANDREWBTC707.github.io/fonts/Poppins/Poppins-ThinItalic.ttf"
+  },
+  {
+    "revision": "73df02902ddaeaed08e7250675ce316a",
+    "url": "ANDREWBTC707.github.io/images/metamask-logo.svg"
+  },
+  {
+    "revision": "91c094d099b9f2f84bfdf4dbea679842",
+    "url": "ANDREWBTC707.github.io/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "ANDREWBTC707.github.io/logo.svg"
+  },
+  {
+    "revision": "b3ab437459e8a2796d982d937a7c5455",
+    "url": "ANDREWBTC707.github.io/manifest.json"
+  },
+  {
+    "revision": "69ec7d9942fd9dd12fb5",
+    "url": "ANDREWBTC707.github.io/static/css/main.3f030b18.chunk.css"
+  },
+  {
+    "revision": "69ec7d9942fd9dd12fb5",
+    "url": "ANDREWBTC707.github.io/static/js/main.69ec7d99.chunk.js"
+  },
+  {
+    "revision": "072d91c5856c8b1fbfc8",
+    "url": "ANDREWBTC707.github.io/static/js/runtime~main.072d91c5.js"
+  },
+  {
+    "revision": "e1235ae68035252654ae",
+    "url": "ANDREWBTC707.github.io/static/js/vendors~main.e1235ae6.chunk.js"
+  },
+  {
+    "revision": "093ee89be9ede30383f39a899c485a82",
+    "url": "ANDREWBTC707.github.io/static/media/Poppins-Regular.35d26b78.ttf"
+  },
+  {
+    "revision": "6f1520d107205975713ba09df778f93f",
+    "url": "ANDREWBTC707.github.io/static/media/Poppins-SemiBold.ac8d04b6.ttf"
+  }
+]);
