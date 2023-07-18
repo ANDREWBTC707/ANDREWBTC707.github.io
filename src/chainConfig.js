9f07625b63f70b1ed6d6c5df0fef5acb417369f3
EvanPiro <dev.evanpiro@gmail.com>
2023-07-18 19:29:57 -0400

    support binance testnet with test contract

--- src/chainConfig.js
+++ src/chainConfig.js
@@ -7,6 +7,12 @@ export const config = {
     stablecoinAddress: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
     scanURL: "https://sepolia.etherscan.io",
   },
+  97: {
+    name: "BinanceSmartChainTestnet",
+    algoBullAddress: "0x1E67DB7b119aDdCBAfEe67978183819788B413D8",
+    stablecoinAddress: "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee",
+    scanURL: "https://testnet.bscscan.com/",
+  },
 };
 
 export const networks = () => {
